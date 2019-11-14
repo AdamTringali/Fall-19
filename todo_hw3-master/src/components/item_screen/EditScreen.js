bbac469d14d372e7f9b7ecf0d27c3a0ba3363e2c
AdamTringali <aatringali@gmail.com>
2019-11-14 15:46:04 -0500

    update layout on editscreen

--- todo_hw3-master/src/components/item_screen/EditScreen.js
+++ todo_hw3-master/src/components/item_screen/EditScreen.js
@@ -166,16 +166,16 @@ class EditScreen extends Component {
                 <div className="row" >
                         
 
-                    <div className="input-field col ">
-                            <input id="description" type="text" className="active" onChange={this.onChange} defaultValue={description}/>
-                            <label htmlFor="description">Description</label>
+                    <div className="active input-field col ">
+                            <input id="description" type="text" className="validate" onChange={this.onChange} defaultValue={description}/>
+                            <label className="active" htmlFor="description">Description</label>
                     </div>
                 </div>
                 <div className="row" >
 
-                    <div className="input-field col">
+                    <div className="active input-field col">
                         <input id="assigned_to" type="text" className="validate" onChange={this.onChange} defaultValue={assigned_to}/>
-                        <label htmlFor="assigned_to">Assigned To</label>
+                        <label className="active" htmlFor="assigned_to">Assigned To</label>
                     </div>
                 </div>
                 <div className="row" >
